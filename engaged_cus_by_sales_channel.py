ca74971d24567586d2cc3bb2a9871e89dbb5ba97
William Rivera <rivera.email@gmail.com>
2019-06-10 20:36:53 -0400

    Update engaged_cus_by_sales_channel.py

--- engaged_cus_by_sales_channel.py
+++ engaged_cus_by_sales_channel.py
@@ -3,12 +3,12 @@ import pandas as pd
 
 df = pd.read_csv('Data/WA_Fn-UseC_-Marketing-Customer-Value-Analysis.csv')
 
-# Count
-by_sales_channel_df = df.loc[  
-df['Response'] == 'Yes',].groupby(['Sales Channel' # engaged customers grouped by renewal offer type
+by_offer_type_df = df.loc[
+# count only engaged customers    
+df['Response'] == 'Yes',].groupby(['Sales Channel' # engaged customers grouped by sales channel
 ]).count()['Customer'] / df.groupby('Sales Channel').count()['Customer']
 
-ax = (by_sales_channel_df * 100.0).plot(
+ax = (by_offer_type_df * 100.0).plot(
 kind = 'bar',
 figsize = (7, 7),
 color = 'skyblue',
